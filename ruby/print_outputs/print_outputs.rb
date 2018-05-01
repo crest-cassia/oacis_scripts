36f5e3c74831ab6d4394e767afb5187614f45e5a
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2018-05-01 18:38:48 +0900

    Clean print_outputs.rb

--- ruby/print_outputs/print_outputs.rb
+++ ruby/print_outputs/print_outputs.rb
@@ -18,6 +18,39 @@ def print_on_parameter_set_headers(outputs_info, variable_info_array, output_inf
   $stdout.puts header2
 end
 
+def get_variables(variable_info_array)
+  variable_info_array.map {|variable| parameter_set.v[variable["name"]]}
+end
+
+def get_averages_of(outputs_array)
+  outputs_array.reduce {|result, outputs| result.zip(outputs).map {|result_output| result_output[0] + result_output[1]}}.map {|sum| sum / outputs_array.length}
+end
+
+def get_errors_of(outputs_array, averages)
+  outputs_array.reduce {|result, outputs| result.zip(outputs, averages).map {|result_output_average| result_output_average[0] + (result_output_average[1] - result_output_average[2]) ** 2}}.map {|value| Math.sqrt(value / outputs_array.length / (outputs_array.length-1))}
+end
+
+def print_averages_errors(variable_info_array, outputs_array)
+  variables = get_variables(variable_info_array)
+
+  averages = get_averages_of(outputs_array)
+  errors = get_errors_of(outputs_array, averages)
+
+  $stdout.print variables[0]
+  variables[1..-1].each {|variable| $stdout.print " #{variable}"}
+  averages.zip(errors).each {|average_error| $stdout.print " #{average_error[0]} #{average_error[1]}"}
+  $stdout.puts
+end
+
+def print_outputs(variable_info_array, outputs)
+  variables = get_variables(variable_info_array)
+
+  $stdout.print variables[0]
+  variables[1..-1].each {|variable| $stdout.print " #{variable}"}
+  outputs.each {|output| $stdout.print " #{output}"}
+  $stdout.puts
+end
+
 
 if ARGV.length < 2 or ARGV.length > 3 then
   $stderr.puts "wrong number of arguments: print_outputs.rb <simulator name> <absolute path of json file> [<analyzer name>]"
@@ -31,20 +64,14 @@ variable_info_array = outputs_info["variables"]
 output_info_array = outputs_info["outputs"]
 
 
+constants_hash = outputs_info["constants"].reduce({}) {|result, (key, value)| result["v.#{key}"] = value; result}
 if ARGV.length == 2 then
   print_on_run_headers(outputs_info, variable_info_array, output_info_array)
 
-  simulator.parameter_sets.where(outputs_info["constants"].reduce({}) {|result, (key, value)| result["v.#{key}"] = value; result}).each do |parameter_set|
-    variables = variable_info_array.map {|variable| parameter_set.v[variable["name"]]}
+  simulator.parameter_sets.where(constants_hash).each do |parameter_set|
     outputs_array = parameter_set.runs.where(status: :finished).map {|run| output_json = JSON.load(File.open(run.dir.join("_output.json"))); output_info_array.map {|output_info| output_json[output_info["name"]]}}
 
-    averages = outputs_array.reduce {|result, outputs| result.zip(outputs).map {|result_output| result_output[0] + result_output[1]}}.map {|sum| sum / outputs_array.length}
-    errors = outputs_array.reduce {|result, outputs| result.zip(outputs, averages).map {|result_output_average| result_output_average[0] + (result_output_average[1] - result_output_average[2]) ** 2}}.map {|value| Math.sqrt(value / outputs_array.length / (outputs_array.length-1))}
-
-    $stdout.print variables[0]
-    variables[1..-1].each {|variable| $stdout.print " #{variable}"}
-    averages.zip(errors).each {|average_error| $stdout.print " #{average_error[0]} #{average_error[1]}"}
-    $stdout.puts
+    print_averages_errors(variable_info_array, outputs_array)
   end
 else
   analyzer = simulator.find_analyzer_by_name(ARGV[2])
@@ -52,30 +79,19 @@ else
   if analyzer[:type] == :on_run then
     print_on_run_headers(outputs_info, variable_info_array, output_info_array)
 
-    simulator.parameter_sets.where(outputs_info["constants"].reduce({}) {|result, (key, value)| result["v.#{key}"] = value; result}).each do |parameter_set|
-      variables = variable_info_array.map {|variable| parameter_set.v[variable["name"]]}
+    simulator.parameter_sets.where(constants_hash).each do |parameter_set|
       outputs_array = parameter_set.runs.where(status: :finished).map {|run| output_json = JSON.load(File.open(run.analyses.where(analyzer: analyzer, status: :finished).max_by {|analysis| analysis.created_at}.dir.join("_output.json"))); output_info_array.map {|output_info| output_json[output_info["name"]]}}
 
-      averages = outputs_array.reduce {|result, outputs| result.zip(outputs).map {|result_output| result_output[0] + result_output[1]}}.map {|sum| sum / outputs_array.length}
-      errors = outputs_array.reduce {|result, outputs| result.zip(outputs, averages).map {|result_output_average| result_output_average[0] + (result_output_average[1] - result_output_average[2]) ** 2}}.map {|value| Math.sqrt(value / outputs_array.length / (outputs_array.length-1))}
-
-      $stdout.print variables[0]
-      variables[1..-1].each {|variable| $stdout.print " #{variable}"}
-      averages.zip(errors).each {|average_error| $stdout.print " #{average_error[0]} #{average_error[1]}"}
-      $stdout.puts
+      print_averages_errors(variable_info_array, outputs_array)
     end
   else# if analyzer[:type] == :on_parameter_set then
     print_on_parameter_set_headers(outputs_info, variable_info_array, output_info_array)
 
-    simulator.parameter_sets.where(outputs_info["constants"].reduce({}) {|result, (key, value)| result["v.#{key}"] = value; result}).each do |parameter_set|
-      variables = variable_info_array.map {|variable| parameter_set.v[variable["name"]]}
+    simulator.parameter_sets.where(constants_hash).each do |parameter_set|
       output_json = JSON.load(File.open(parameter_set.analyses.where(analyzer: analyzer, status: :finished).max_by {|analysis| analysis.created_at}.dir.join("_output.json")))
       outputs = output_info_array.map {|output_info| output_json[output_info["name"]]}
 
-      $stdout.print variables[0]
-      variables[1..-1].each {|variable| $stdout.print " #{variable}"}
-      outputs.each {|output| $stdout.print " #{output}"}
-      $stdout.puts
+      print_outputs(variable_info_array, outputs)
     end
   end
 end
